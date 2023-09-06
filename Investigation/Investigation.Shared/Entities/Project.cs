6f0863680e224733721fdeb7b00ff9f72c6def86
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-09-05 21:20:21 -0500

    Creation of research projects and Entities

---
+++ Investigation/Investigation.Shared/Entities/Project.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Shared.Entities
+{
+    public  class Project
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Proyecto")]
+        [MaxLength(100,ErrorMessage ="El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage ="El campo{0} es obligatorio ")]
+        public string Name { get; set; } = null;
+
+        [Display(Name = "Descripcion")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string Description { get; set; } = null;
+
+        [Display(Name = "Lider")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string Leader { get; set; } = null;
+
+        [Display(Name = "area")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string area { get; set; } = null;
+
+        public DateTime Date { get; set; }
+
+
+
+
+    }
+}
---
+++ Investigation/Investigation.Shared/Entities/investigator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investigation.Shared.Entities
+{
+    public  class Investigator
+
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Afiliacion Institucional")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string membership { get; set; } = null;
+
+        [Display(Name = "Especializacion")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string specialization { get; set; } = null!;
+
+
+
+        [Display(Name = "Rol")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe terner maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo{0} es obligatorio ")]
+        public string role { get; set; } = null!;
+
+
+    }
+}

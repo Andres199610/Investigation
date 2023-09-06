d0deb14745fbc370020f257ae71807671245ce93
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-09-05 20:51:51 -0500

    First version

---
+++ Investigation/Investigation.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Investigation.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
